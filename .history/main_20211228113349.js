8eeda45a17eee7b3f5325171836515c57bd9a9b8
MarkLee <1643565650@qq.com>
2021-12-28 11:59:00 +0800

    split分隔数组

---
+++ .history/main_20211228113349.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3];
+
+arr["whatEver"] = "Hello World!";
+
+console.log(arr);
---
+++ .history/main_20211228114938.js
@@ -0,0 +1,3 @@
+let arr = "1,2,3".split();
+let arr2 = "1,2,3".split("");
+let arr3 = "1,2,3".split(",");
