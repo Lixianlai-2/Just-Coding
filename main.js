714d5f101a1ec54c8a54fd91d6599c8a519a6453
MarkLee <1643565650@qq.com>
2022-01-04 19:43:43 +0800

    彻底移出

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ test1.style.fontSize = "100px";
 
 let div2 = test1.cloneNode(true);
 document.body.appendChild(div2);
+
+// div.parentNode.removeChild(div);
+div.remove();
+div = null;
+test2.appendChild(div);
