afb2eabf3d46a39ebd250b2cf0797e491f1875dc
MarkLee <1643565650@qq.com>
2022-01-10 22:33:45 +0800

    爷爷父亲儿子

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-let div1 = document.querySelector("div");
+let grandpa = document.querySelector(".grandpa");
+let father = document.querySelector(".father");
+let son = document.querySelector(".son");
 
-div1.id = "Lixianlai";
-div1.x = "Lixianlai";
-div1.dataset.x = "Lixianlai";
+son.addEventListener("click", (Event) => {
+  const savedCurrentTarget = Event.currentTarget;
+
+  setTimeout(() => {
+    console.log(Event.currentTarget);
+  }, 0);
+});
+
+// 冒泡
+// son.addEventListener("click", (Event) => {
+//   console.log("儿子打印出来了吗");
+// });
+
+// father.addEventListener("click", (Event) => {
+//   console.log("父亲打印出来了吗");
+//   Event.stopPropagation();
+// });
+
+// grandpa.addEventListener("click", (Event) => {
+//   console.log("爷爷有打印出来吗");
+// });
